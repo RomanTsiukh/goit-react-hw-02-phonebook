0e09316203f3e65590ef8d1771dfadc48ebc4191
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-08-12 16:32:11 +0300

    fixed

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Filter = ({ value, onChange }) => {
-  <label>
-    Filter
-    <input type="text" value={value} onChange={onChange} />;
-  </label>;
+  return (
+    <label>
+      Filter
+      <input type="text" value={value} onChange={onChange} />;
+    </label>
+  );
 };
 
 export default Filter;
